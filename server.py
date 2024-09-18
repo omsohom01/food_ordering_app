7d24a899aaca060a8201306074bc33a3b2751fb1
Sohom <omsohom01@gmail.com>
2024-09-18 15:33:26 +0530

    server

--- server.py
+++ server.py
@@ -17,15 +17,17 @@ def serve_image(filename):
 @app.route('/submit_order', methods=['POST'])
 def submit_order():
     data = request.json
+    order_summary = data['order_summary']
     name = data['name']
     roll_number = data['roll_number']
     department = data['department']
-    order_details = data['order_details']
+    total_price = data['total_price']
 
-    response_message = f"{order_details}Ordered by {name}, Roll No: {roll_number}, Department: {department}."
+    response_message = f"Order Summary:<br>{order_summary}<br><strong>Name:</strong> {name}<br><strong>Roll Number:</strong> {roll_number}<br><strong>Department:</strong> {department}<br><strong>Total Bill:</strong> ₹{total_price}/-"
     print(response_message)  # Server logs the order
     return jsonify({"message": response_message})
 
 if __name__ == '__main__':
+    # Ensure the app listens on the port specified by Render
     port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
